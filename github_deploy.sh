4a458b4ffbf6d88d9df453f1204f29c5bfe45a8c
XiangMin Liu <635750556@qq.com>
2023-02-18 02:49:26 +0800

    234234

---
+++ github_deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# 忽略错误
+set -e
+
+# 构建
+npm run build
+
+# 进入待发布的目录
+cd .vitepress/dist
+
+# 如果是发布到自定义域名
+echo 'www.queryphp.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果是部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:hunzhiwange/docs-cn.git master:gh-pages
+
+# 回到之前的目录
+cd -
